e7ff51a9dd881af7344ced280592efc47c93ade6
Yudhajit Chowdhury <59924789+YudhajitChowdhury@users.noreply.github.com>
2020-08-14 00:43:17 +0530

    Update CountingRockSample.java

--- CountingRockSample.java
+++ CountingRockSample.java
@@ -37,3 +37,39 @@ Output: 1 12 20
 Explanation:
 
 There are 20 samples and 3 ranges. The samples are 921, 107 195. The ranges are 1-100, 50-600 and 1-1000. Note that the ranges are overlapping. The number of samples in each of the three ranges are 1, 12 and 20 respectively. Hence the three lines of the output are 1, 12 and 20.*/
+
+
+
+import java.util.Scanner;
+ class Main {
+ 
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int[] a = new int[1000];
+        int s,i,j,t,l1=0,l2=0,c=0;
+        
+        System.out.println("Enter the no of sample");
+        s = sc.nextInt();
+        
+        System.out.println("Enter the no of range");
+        t = sc.nextInt();
+        
+        System.out.println("Enter the numbers");
+        for (i = 0; i < s; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        for (i = 0; i< t; i++) {
+            System.out.println("Enter the max and min range");
+                l1 = sc.nextInt();
+                l2 = sc.nextInt();
+                for (j = 0; j < s; j++) { if((a[j]>=l1)&&(a[j]<=l2))
+                        c++;            
+                }
+                System.out.println(c);
+                c=0;
+        }
+        
+        
+    }
