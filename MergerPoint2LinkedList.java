3081ff52576135346007392ad7eb76e62ee45f6b
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2020-09-06 14:00:17 +0530

    Create MergerPoint2LinkedList.java

---
+++ MergerPoint2LinkedList.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        
+    SinglyLinkedListNode head1_current = head1;
+    SinglyLinkedListNode head2_current = head2;
+
+    while(head1_current != head2_current){
+        if(head1_current.next==null){
+            head1_current.next  = head2;
+        }
+        else
+            head1_current = head1_current.next;
+
+        if(head2_current.next==null){
+            head2_current.next  = head1;
+        }
+        else
+            head2_current = head2_current.next;
+        
+    }
+
+    return head1_current.data;
+
+}
+
+   
+
+    private static final Scanner scanner = new Scanner(System.in);
